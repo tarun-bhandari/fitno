0a6e64ba2bfb6327e110932b06cbcddf00a9f413
tarun-bhandari <iamtarunbhandari07@gmail.com>
2024-03-01 16:15:33 +0530

    removing initial code

--- app/src/main/java/com/tarunbhandari/fitno/MainActivity.kt
+++ app/src/main/java/com/tarunbhandari/fitno/MainActivity.kt
@@ -1,46 +1,24 @@
 package com.tarunbhandari.fitno
 
+/*
+    @Author Tarun Bhandari
+    github: https://github.com/tarun-bhandari/
+ */
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.tarunbhandari.fitno.ui.theme.FitnoTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FitnoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitnoTheme {
-        Greeting("Android")
-    }
-}
