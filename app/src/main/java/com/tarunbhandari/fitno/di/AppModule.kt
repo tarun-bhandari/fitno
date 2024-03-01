d5e8c8eab6e8072b3a144f9f1d4f601381fcf659
tarun-bhandari <iamtarunbhandari07@gmail.com>
2024-03-01 18:49:26 +0530

    added sharedpreference dependency

---
+++ app/src/main/java/com/tarunbhandari/fitno/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.tarunbhandari.fitno.di
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(context: Context): SharedPreferences {
+        return context.getSharedPreferences("Fitno_Shared_Preferences", Context.MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun provideApplicationContext(application: Application): Context {
+        return application.applicationContext
+    }
+
+}
+
+
