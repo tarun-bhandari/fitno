3a6388b5d91f1124317bc3c486559af3050d37fc
tarun-bhandari <iamtarunbhandari07@gmail.com>
2024-03-02 02:05:37 +0530

    setup navigation

--- app/src/main/java/com/tarunbhandari/fitno/util/Screen.kt
+++ app/src/main/java/com/tarunbhandari/fitno/util/Screen.kt
@@ -3,5 +3,5 @@ package com.tarunbhandari.fitno.util
 sealed class Screen(val route: String) {
     object OnBoarding: Screen("onboarding")
     object Login: Screen("onboarding")
-    object Home: Screen("onboarding")
+    object Home: Screen("home")
 }
