bfa04c61c882bf6793070d7a28ab788aab5651ee
tarun-bhandari <iamtarunbhandari07@gmail.com>
2024-03-01 19:37:28 +0530

    added edgetoedge

--- app/src/main/java/com/tarunbhandari/fitno/MainActivity.kt
+++ app/src/main/java/com/tarunbhandari/fitno/MainActivity.kt
@@ -5,18 +5,27 @@ package com.tarunbhandari.fitno
     github: https://github.com/tarun-bhandari/
  */
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.tarunbhandari.fitno.ui.screen.Home.HomeScreen
 import com.tarunbhandari.fitno.ui.theme.FitnoTheme
 
-
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+//         For the edge to edge
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
         super.onCreate(savedInstanceState)
         setContent {
             FitnoTheme {
-
+                HomeScreen()
             }
         }
     }
