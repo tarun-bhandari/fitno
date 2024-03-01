0f1fa167e37feb763bf95561b657999a26ae6550
tarun-bhandari <iamtarunbhandari07@gmail.com>
2024-03-01 16:59:17 +0530

    added screens

---
+++ app/src/main/java/com/tarunbhandari/fitno/util/Screen.kt
@@ -0,0 +1,7 @@
+package com.tarunbhandari.fitno.util
+
+sealed class Screen(val route: String) {
+    object OnBoarding: Screen("onboarding")
+    object Login: Screen("onboarding")
+    object Home: Screen("onboarding")
+}
