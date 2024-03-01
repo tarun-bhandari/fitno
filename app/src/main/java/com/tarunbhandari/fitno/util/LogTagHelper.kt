f524a83b8ed9bd54b640bd6697a38d9351635793
tarun-bhandari <iamtarunbhandari07@gmail.com>
2024-03-02 02:05:06 +0530

    added helper class

---
+++ app/src/main/java/com/tarunbhandari/fitno/util/LogTagHelper.kt
@@ -0,0 +1,8 @@
+package com.tarunbhandari.fitno.util
+
+// This contain all the tag which i am gonna use in the Log for my understanding
+object LogTagHelper {
+    const val NAV = "Navigation"
+    const val CLICK = "Button Click"
+    const val SCREEN = "Screen"
+}
