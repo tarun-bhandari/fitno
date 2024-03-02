e24ef7ec8de2a957de14488adf8eff65f7538b93
tarun-bhandari <iamtarunbhandari07@gmail.com>
2024-03-02 11:06:23 +0530

    added database dependency

--- app/src/main/java/com/tarunbhandari/fitno/di/AppModule.kt
+++ app/src/main/java/com/tarunbhandari/fitno/di/AppModule.kt
@@ -3,10 +3,13 @@ package com.tarunbhandari.fitno.di
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import com.tarunbhandari.fitno.data.AppDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 @Module
@@ -24,6 +27,15 @@ object AppModule {
     fun provideApplicationContext(application: Application): Context {
         return application.applicationContext
     }
+
+    @Provides
+    fun provideCoroutineScope(): CoroutineScope = CoroutineScope(SupervisorJob())
+
+    @Provides
+    fun provideAppDatabase(context: Context, scope: CoroutineScope): AppDatabase {
+        return AppDatabase.getDatabase(context, scope)
+    }
+
 }
 
 
