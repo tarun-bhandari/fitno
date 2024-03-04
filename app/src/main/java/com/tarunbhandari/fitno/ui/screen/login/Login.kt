fdf44cffbc80b948534a1bb34e28d245591ff4d6
tarun-bhandari <iamtarunbhandari07@gmail.com>
2024-03-04 21:47:18 +0530

    done with the account preview design

--- app/src/main/java/com/tarunbhandari/fitno/ui/screen/login/Login.kt
+++ app/src/main/java/com/tarunbhandari/fitno/ui/screen/login/Login.kt
@@ -1,19 +1,32 @@
 package com.tarunbhandari.fitno.ui.screen.login
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -110,10 +123,24 @@ fun LoginScreen() {
 
 @Composable
 fun UserAccountScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize()
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth(0.9f)
+            .border(shape = MaterialTheme.shapes.small, color = Color.Black, width = 3.dp)
+            .padding(16.dp)
     ) {
-
+        Image(
+            imageVector = Icons.Default.AccountCircle,
+            contentDescription = "User Icon",
+            contentScale = ContentScale.Fit
+        )
+        Text(
+            text = "Tarun Bhandari",
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp
+        )
     }
 }
 
@@ -132,12 +159,21 @@ fun LoginScreenPreview() {
 
 @Composable
 @Preview(
-    showSystemUi = true,
     showBackground = true,
     name = "Already login user account"
 )
 fun UserAccountScreenPreview() {
     FitnoTheme {
-        UserAccountScreen()
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp)
+        ) {
+            UserAccountScreen()
+            UserAccountScreen()
+            UserAccountScreen()
+        }
     }
 }
